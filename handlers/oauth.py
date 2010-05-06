b940bd73942fefa0a83536fdc8c7e8df24ee9d3b
Bob Aman <bobaman@google.com>
2010-05-05 17:01:51 -0700

    Adding copyright statements.

--- handlers/oauth.py
+++ handlers/oauth.py
@@ -1,3 +1,17 @@
+# Copyright 2010 Google Inc.
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#      http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import yaml
 
 from google.appengine.ext import db
@@ -20,7 +34,6 @@ class OAuthInitHandler(webapp.RequestHandler):
   @property
   def client(self):
     if not hasattr(self, '_client') or not self._client:
-      logging.info('building client...')
       self._client = buzz.Client()
       self._client.oauth_consumer_key = OAUTH_CONSUMER_KEY
       self._client.oauth_consumer_secret = OAUTH_CONSUMER_SECRET
